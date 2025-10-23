d6c7866a708e4a2328dae30ad889dade13412e86
Malak Ettabia <malakettabia00@gmail.com>
2025-10-23 18:48:32 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#from turtle import Turtle, Screen
+#
+#timmy = Turtle()
+#print(timmy)
+#timmy.shape("turtle")
+#timmy.color("blue")
+#timmy.forward(100)
+#
+#my_screen = Screen()
+#print(my_screen.canvheight)
+#my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.field_names = ["Pokemon Name", "Type"]
+table.add_rows(
+    [
+        ["Pikachu", "Electric"],
+        ["Squirle", "Water"]
+    ]
+)
+table.align = "l"
+print(table)
